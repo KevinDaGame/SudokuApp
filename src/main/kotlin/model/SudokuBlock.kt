0b6a7d39fe8d50ec015faffc7f58d609117ee379
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2023-05-22 17:15:40 +0200

    Setup show board

--- src/main/kotlin/model/SudokuBlock.kt
+++ src/main/kotlin/model/SudokuBlock.kt
@@ -1,4 +1,11 @@
 package model
 
 class SudokuBlock(cells: List<SudokuCell>) : SudokuGroup(cells) {
+    fun isLastX(x: Int, y: Int): Boolean {
+        return cells.filter { it.y == y }.none { it.x > x }
+    }
+
+    fun isLastY(x: Int, y: Int): Boolean {
+        return cells.filter { it.x == x }.none { it.y > y }
+    }
 }
--- src/main/kotlin/view/SudokuView.kt
+++ src/main/kotlin/view/SudokuView.kt
@@ -2,38 +2,96 @@ package view
 
 import controller.SudokuController
 import model.SudokuCell
+import java.lang.StringBuilder
 
 class SudokuView(private val controller: SudokuController) : IView {
-    var x: Int = 1
-    var y: Int = 1
+    var x: Int = 0
+    var y: Int = 0
+
     init {
-        while(true) {
+        while (true) {
             render()
             getInput()
         }
     }
+
     override fun render() {
         val blocks = controller.getBlocks()
         val board = Array(controller.model.height) { arrayOfNulls<SudokuCell>(controller.model.width) }
+        val lines = Array(controller.model.height) { Array(controller.model.width) { Pair(false, false) } }
 
-        for (cell in blocks.flatMap { it.cells }) {
-            board[cell.y][cell.x] = cell
+        for (block in blocks) {
+            for (cell in block.cells) {
+                board[cell.y][cell.x] = cell
+                var borderX = false
+                var borderY = false
+                if (cell.x < controller.model.width && block.isLastX(cell.x, cell.y)) {
+                    borderX = true
+                }
+                if (cell.y < controller.model.height && block.isLastY(cell.x, cell.y)) {
+                    borderY = true
+                }
+                lines[cell.y][cell.x] = Pair(borderX, borderY)
+
+            }
         }
-        println("-".repeat(controller.model.width))
-        for (row in board) {
-            print("|")
-            for (cell in row) {
-                if (cell != null) {
-                    print(cell.value.value)
+
+        printHorizontalBorder(controller.model.width)
+        for (i in 0..controller.model.height - 1) {
+            printRow(board[i], lines[i])
+
+        }
+        println("x: $x, y: $y")
+
+    }
+
+    private fun printHorizontalBorder(width: Int) {
+        println("|-----|-----|-----|")
+    }
+
+    private fun printRow(row: Array<SudokuCell?>, line: Array<Pair<Boolean, Boolean>>) {
+        val horizontalLine = StringBuilder()
+        var isFirst = true
+        for (cell in row) {
+            if (cell != null) {
+                if (isFirst) {
+                    print("|")
+                    horizontalLine.append("|")
+                    isFirst = false
+                }
+                print(cell.value.value)
+
+                val currentLine = line[cell.x]
+                if (currentLine.second) {
+                    if (!currentLine.first) {
+                        horizontalLine.append("-")
+                    }
+                    horizontalLine.append("-")
+                } else {
+                    if (!currentLine.first) {
+                        horizontalLine.append(" ")
+                    }
+                    horizontalLine.append(" ")
+                }
+                if (currentLine.first) {
+                    print("|")
+                    horizontalLine.append("|")
                 } else {
                     print(" ")
                 }
+            } else {
+                if (!isFirst) {
+                    print(" ")
+                    horizontalLine.append(" ")
+                    isFirst = true
+                    continue
+                }
+                print("  ")
+                horizontalLine.append("  ")
             }
-            println("|")
         }
-        println("-".repeat(controller.model.width))
-        println("x: $x, y: $y")
-
+        println()
+        println(horizontalLine)
     }
 
     fun getInput() {

63ef2dd7c28d49e64d286e65654e692efe5db1f7
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2023-05-23 14:50:45 +0200

    View cleanup

--- src/main/kotlin/view/SudokuView.kt
+++ src/main/kotlin/view/SudokuView.kt
@@ -11,81 +11,89 @@ class SudokuView(private val controller: SudokuController) : IView {
 
     override fun render() {
         printSudoku()
-        JTerminal.println("x: $x, y: $y")
-
     }
 
     private fun printSudoku() {
-        JTerminal.println("\n".repeat(100)) // clear screen
-
         val viewModel = SudokuViewModel(controller.getBlocks(), controller.model.width, controller.model.height)
         val board = Array(viewModel.width * 2 + 1) { Array(viewModel.height * 2 + 1) { ConsoleChar() } }
+
         for ((y, row) in viewModel.board.withIndex()) {
-            for ((x, cell) in row.withIndex()) {
-                if (cell != null) {
-                    board[y * 2 + 1][x * 2 + 1] = ConsoleChar(cell.value.value.digitToChar())
-
-
-                    val horizontalBorder = ConsoleChar('-', Color.SILVER)
-                    val verticalBorder = ConsoleChar('|', Color.SILVER)
-                    for (border in cell.borders) {
-                        when (border) {
-                            BorderDirection.UP -> {
-                                board[y * 2][x * 2 + 1] = horizontalBorder
-                                if (!cell.borders.contains(BorderDirection.RIGHT)) {
-                                    board[y * 2][x * 2 + 2] = horizontalBorder
-                                }
-                            }
-
-                            BorderDirection.DOWN -> {
-                                board[y * 2 + 2][x * 2 + 1] = horizontalBorder
-                                if (!cell.borders.contains(BorderDirection.RIGHT)) {
-                                    board[y * 2 + 2][x * 2 + 2] = horizontalBorder
-                                }
-                            }
-
-                            BorderDirection.LEFT -> {
-                                board[y * 2 + 1][x * 2] = verticalBorder
-                                board[y * 2 + 2][x * 2] = verticalBorder
-                                if(cell.borders.contains(BorderDirection.UP)) {
-                                    board[y * 2][x * 2] = verticalBorder
-                                }
-
-                            }
-
-                            BorderDirection.RIGHT -> {
-                                board[y * 2 + 1][x * 2 + 2] = verticalBorder
-                                board[y * 2 + 2][x * 2 + 2] = verticalBorder
-                                if(cell.borders.contains(BorderDirection.UP)) {
-                                    board[y * 2][x * 2 + 2] = verticalBorder
-                                }
-                            }
-                        }
-                    }
-
-
-                }
-
-            }
+            printRow(board, y, row)
         }
 
-        //show cursor
-        board[y * 2][x * 2] = ConsoleChar(board[y*2][x*2].char, background = Color.RED)
-        board[y * 2][x * 2 + 1] = ConsoleChar(board[y*2][x * 2 + 1].char, background = Color.RED)
-        board[y * 2][x * 2 + 2] = ConsoleChar(board[y*2][x * 2 + 2].char, background = Color.RED)
-        board[y * 2 + 1][x * 2] = ConsoleChar(board[y*2 + 1][x * 2].char, background = Color.RED)
-        board[y * 2 + 1][x * 2 + 2] = ConsoleChar(board[y*2 + 1][x * 2 + 2].char, background = Color.RED)
-        board[y * 2 + 2][x * 2] = ConsoleChar(board[y*2 + 2][x * 2].char, background = Color.RED)
-        board[y * 2 + 2][x * 2 + 1] = ConsoleChar(board[y*2 + 2][x * 2 + 1].char, background = Color.RED)
-        board[y * 2 + 2][x * 2 + 2] = ConsoleChar(board[y*2 + 2][x * 2 + 2].char, background = Color.RED)
-
-
+        showCursor(board)
 
         for (line in board) {
             for (cell in line) {
                 JTerminal.print(cell.char.toString(), cell.color, cell.background)
             }
             JTerminal.println("")
+        }
+    }
+
+    private fun showCursor(board: Array<Array<ConsoleChar>>) {
+        for (dx in -1..1) {
+            for(dy in -1..1) {
+                if(dy == 0 && dx == 0) continue
+                board[y * 2 + 1 + dy][x * 2 + 1 + dx] = ConsoleChar(board[y * 2 + 1 + dy][x * 2 + 1 + dx].char, background = Color.RED)
+
+            }
+        }
+    }
+
+    private fun printRow(board: Array<Array<ConsoleChar>>, y: Int, row: Array<ViewSudokuCell?>) {
+        for ((x, cell) in row.withIndex()) {
+            if (cell != null) {
+                board[y * 2 + 1][x * 2 + 1] = ConsoleChar(cell.value.value.digitToChar())
+
+
+
+                addBorders(cell, board, y, x)
+
+
+
+            }
+
+        }
+
+    }
+
+    private fun addBorders(cell: ViewSudokuCell, board: Array<Array<ConsoleChar>>, y: Int, x: Int) {
+        val horizontalBorder = ConsoleChar('-', Color.SILVER)
+        val verticalBorder = ConsoleChar('|', Color.SILVER)
+        for (border in cell.borders) {
+            when (border) {
+                BorderDirection.UP -> {
+                    board[y * 2][x * 2 + 1] = horizontalBorder
+                    if (!cell.borders.contains(BorderDirection.RIGHT)) {
+                        board[y * 2][x * 2 + 2] = horizontalBorder
+                    }
+                }
+
+                BorderDirection.DOWN -> {
+                    board[y * 2 + 2][x * 2 + 1] = horizontalBorder
+                    if (!cell.borders.contains(BorderDirection.RIGHT)) {
+                        board[y * 2 + 2][x * 2 + 2] = horizontalBorder
+                    }
+                }
+
+                BorderDirection.LEFT -> {
+                    board[y * 2 + 1][x * 2] = verticalBorder
+                    board[y * 2 + 2][x * 2] = verticalBorder
+                    if (cell.borders.contains(BorderDirection.UP)) {
+                        board[y * 2][x * 2] = verticalBorder
+                    }
+
+                }
+
+                BorderDirection.RIGHT -> {
+                    board[y * 2 + 1][x * 2 + 2] = verticalBorder
+                    board[y * 2 + 2][x * 2 + 2] = verticalBorder
+                    if (cell.borders.contains(BorderDirection.UP)) {
+                        board[y * 2][x * 2 + 2] = verticalBorder
+                    }
+                }
+            }
         }
     }
 
--- src/main/kotlin/view/ViewManager.kt
+++ src/main/kotlin/view/ViewManager.kt
@@ -1,11 +1,13 @@
 package view
 
+import io.github.shuoros.jterminal.JTerminal
 import java.util.*
 
 class ViewManager private constructor(var activeView: IView = MainView()) {
     fun start() {
         val scanner = Scanner(System.`in`)
-        while(true) {
+        while (true) {
+            flushView()
             activeView.render()
             activeView.handleInput(scanner.next().first())
 
@@ -13,10 +15,14 @@ class ViewManager private constructor(var activeView: IView = MainView()) {
         }
     }
 
-companion object {
+    private fun flushView() {
+        JTerminal.println("\n".repeat(100)) // clear screen
+    }
+
+    companion object {
         val instance: ViewManager = ViewManager()
 
-}
+    }
 
 }
 
